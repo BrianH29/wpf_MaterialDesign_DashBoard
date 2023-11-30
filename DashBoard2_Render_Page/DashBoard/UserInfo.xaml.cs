b0e8f7fdc180fef34d6855f8a4cfb20cdf87d50c
Brian Hwang <brianhwang29@gmail.com>
2023-11-30 14:45:10 +0900

    render 2 page with renderpage interface & Frame tag

---
+++ DashBoard2_Render_Page/DashBoard/UserInfo.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DashBoard
+{
+    /// <summary>
+    /// Interaction logic for UserInfo.xaml
+    /// </summary>
+    public partial class UserInfo : UserControl
+    {
+        public UserInfo()
+        {
+            InitializeComponent();
+
+            string imgCartoon = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString()}\\Images\\cartoon-woman-pretty.png";
+            string imgavatar = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString()}\\Images\\avatar1.jpg";
+            ImageCartoon.Source = new BitmapImage(new Uri(imgCartoon));
+            avatar1.Source = new BitmapImage(new Uri(imgavatar));
+            avatar2.Source = new BitmapImage(new Uri(imgavatar));
+        }
+    }
+}
